2c570ddab6cedba722f3bb8a42393a88c3d16fdf
Bekir Bilgili <mbekirbilgili@gmail.com>
2024-03-22 21:32:12 +0300

    Initial Commit

--- Book Sorter/src/Main.java
+++ Book Sorter/src/Main.java
@@ -3,14 +3,34 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        TreeSet<Book> bookList = new TreeSet<>(new SortByPageNumber());
+        HashSet<Book> bookList = new HashSet<>();// önce tüm kitaplar bir hashsette toplandı
+                                                //sonrasında sıralama için yeni treesetlere atanacak
 
-        bookList.add(new Book("Küçük Prens",112,"Antoine de Saint-Exupéry","1943"));
+        bookList.add(new Book("Küçük Prens",112,"Antoine de Saint-Exupéry","1943")); //Book sınıfondan 5 tane kitap oluşturuldu
         bookList.add(new Book("Harry Potter",276,"J K Rowling","1997"));
         bookList.add(new Book("Savaş ve Barış",184,"Tolstoy","1869"));
-        bookList.add(new Book("Simyacı",112,"Coelho","1988"));
+        bookList.add(new Book("Simyacı",190,"Coelho","1988"));
         bookList.add(new Book("Iki Şehrin Hikayesi",464,"Charles Dickens","1859"));
 
+        System.out.println("------- Sayfa Sayısına Göre Sıralama -----------");
+        TreeSet<Book> bookListPageNumber = new TreeSet<>(new SortByPageNumber());// Sort by number sınıfı comparator dan implemnte edildi
+        bookListPageNumber.addAll(bookList); // sayfa sayısına göre sıralama için yeni bir tree set oluşturuldu.
+
+        for (Book book1: bookListPageNumber){
+            System.out.println(book1.getName());
+        }
+        System.out.println();
+
+        System.out.println("----------------------------------------");
+        System.out.println();
+
+        System.out.println("------- İsim Sırasına Göre Sıralama -----------");
+        TreeSet<Book> bookListAlphabetic = new TreeSet<>(new SortByName());// Sort by name sınıfı comparator dan implemnte edildi
+        bookListAlphabetic.addAll(bookList); // alfabetik göre sıralama için yeni bir tree set oluşturuldu.
+
+        for(Book book2: bookListAlphabetic){
+            System.out.println(book2.getName());
+        }
 
 
     }
--- Book Sorter/src/SortByPageNumber.java
+++ Book Sorter/src/SortByPageNumber.java
@@ -4,6 +4,6 @@ public class SortByPageNumber implements Comparator<Book> {
 
     @Override
     public int compare(Book o1, Book o2) {
-        return 0;
+        return o1.getPageNumber()- o2.getPageNumber();
     }
 }
